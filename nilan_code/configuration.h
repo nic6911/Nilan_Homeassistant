f2d92b7dcad90d6e8be0feb34fefb66e0265ae78
Mogens <mogens@ustepper.com>
2021-04-14 08:26:51 +0200

    ..

--- nilan_code/configuration.h
+++ nilan_code/configuration.h
@@ -6,10 +6,10 @@
 
 // WIFI settings
 #define WIFISSID     "My Wi-Fi Network" // Put in your SSID 
-#define WIFIPASSWORD "24092017" // Put in you SSID Password 
+#define WIFIPASSWORD "password" // Put in you SSID Password 
 #define CUSTOM_HOSTNAME "NilanGW-%s"  // Host name of the ESP8266 so that it's easier to find in your DHCP range
 
 // MQTT settings
 #define MQTTSERVER   "192.168.0.100" // Put in the IP adresse of your MQTT broker
 #define MQTTUSERNAME "mqtt" // User name for the MQTT broker (NULL if no user name is required)
-#define MQTTPASSWORD "240917" // Password for the MQTT broker (NULL if no password is required)
+#define MQTTPASSWORD "password" // Password for the MQTT broker (NULL if no password is required)
